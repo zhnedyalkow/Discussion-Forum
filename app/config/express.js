d9ccd9a28db1056130db58b44939a453c1a4a573
DESKTOP-UTDKDOP\zhi <zh.nedyalkow@gmail.com>
2018-03-19 10:28:27 +0200

    Changed README.md file, added server.js and config files. Created master.pug, nav.pug.

---
+++ app/config/express.js
@@ -0,0 +1,22 @@
+/* globals __dirname */
+
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// const init = (app, data) => {
+const init = (app) => {
+    app.use(bodyParser.urlencoded({
+        extended: true,
+    }));
+
+    app.use('/static', express.static(path.join(__dirname, '../../public')));
+    app.use(morgan('combined'));
+    app.set('view engine', 'pug');
+};
+
+module.exports = {
+  init,  
+};
--- app/server.js
+++ app/server.js
@@ -1,1 +1,11 @@
-var asd = 5;
+const express = require('express');
+// const data = require('./data');
+const app = express();
+const port = 3001;
+
+require('./config/express').init(app);
+// require('./routes').init(app, data);
+require('./routes').init(app);
+
+app.listen(port);
+console.log(`App running at port: ${port}`);
