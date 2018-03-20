d47a21c9f91d2c547304d50342aa70c4236f26e5
Mirela <mirelasteve@abv.bg>
2018-03-20 10:26:17 +0200

    created login and sgn up forms

--- app/server.js
+++ app/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 // const data = require('./data');
 const app = express();
+const data = require('./routes/data');
 const port = 3001;
 
 require('./config/express').init(app);
+require('./config/auth').init(app);
 // require('./routes').init(app, data);
-require('./routes').init(app);
+require('./routes').init(app, data);
+require('./routes/auth.routes').init(app, data);
 
 app.listen(port);
 console.log(`App running at port: ${port}`);
