a33b566f6edff75db7154c4f6dfca4e0509930bc
valio <Valios2@hotmail.com>
2018-03-20 12:50:45 +0200

    eslint fixes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,44 +1,41 @@
-/* globals __dirname __filename */
-
-const fs = require('fs');
-const path = require('path');
-const passport = require('passport');
+const {
+    Router,
+} = require('express');
 
 const init = (app, data) => {
-    // const init = (app) => {
-    app.get('/', async(req, res) => {
-        // const viewName = '../../views/home';
-        const viewName = '../../views/forum/home';
+    const router = new Router();
+    router
+        .get('/', async (req, res) => {
+            const viewName = '../../views/forum/home';
 
-        res.render(viewName);
-    });
+            res.render(viewName);
+        })
+        .get('/', async (req, res) => {
+            const viewName = '../../views/forum/login';
 
-    app.get('/', async(req, res) => {
-        const viewName = '../../views/forum/login';
-
-        res.render(viewName);
-    });
-    app.get('/sign-up', async(req, res) => {
-        const viewName = '../../views/forum/sign-up';
-        res.render(viewName);
-    });
-    app.post('/sign-up', async(req, res) => {
-        const email = req.body.email;
-        const username = req.body.username;
-        const password = req.body.password;
-        const firstName = req.body.firstName;
-        const lastName = req.body.lastName;
-        const userRole = req.body.userRole;
-        await data.create(email, username, password, firstName, lastName, userRole);
-        console.log(req.body);
-        const viewName = '../../views/forum/sign-up';
-        res.redirect('/sign-up');
-    });
-    app.get('/myprofile', async(req, res) => {
-        const viewName = '../../views/forum/privateLogin';
-        res.render(viewName);
-    });
-}
+            res.render(viewName);
+        })
+        .get('/sign-up', async (req, res) => {
+            const viewName = '../../views/forum/sign-up';
+            res.render(viewName);
+        })
+        .post('/sign-up', async (req, res) => {
+            const email = req.body.email;
+            const username = req.body.username;
+            const password = req.body.password;
+            const firstName = req.body.firstName;
+            const lastName = req.body.lastName;
+            const userRole = req.body.userRole;
+            await data.create(email, username, password,
+                firstName, lastName, userRole);
+            console.log(req.body);
+            res.redirect('/sign-up');
+        })
+        .get('/myprofile', async (req, res) => {
+            const viewName = '../../views/forum/privateLogin';
+            res.render(viewName);
+        });
+};
 
 /** dynamically load all routes */
 
