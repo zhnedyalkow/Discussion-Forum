3b98a368a6f452e0cd6559c5ae4ee7a56624661a
Mirela <mirelasteve@abv.bg>
2018-03-23 12:19:40 +0200

    add button for save and answer and filling the database

--- public/js/jQuery.js
+++ public/js/jQuery.js
@@ -1,7 +1,12 @@
 $(function() {
     $("#answer").on("click", function() {
         $("#answerUl")
-            .append('<div><input type="textarea" rows="6"  id="answerLi"></div>');
+            .append(`<div>
+            <form method='post' >
+            <input type="textarea" rows="6" name="msg"  id="answerLi">
+            <button type="submit" id="saveButton" class="btn btn-default btn-lg" formmethod="post">Save</button>
+            </form>
+            </div>`);
     })
 
 });
