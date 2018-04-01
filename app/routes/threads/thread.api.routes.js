366d09107e802c9cceb73123f942fb97e98acf73
Mirela <mirelasteve@abv.bg>
2018-04-01 10:05:39 +0300

    merge

--- app/routes/threads/thread.api.routes.js
+++ app/routes/threads/thread.api.routes.js
@@ -12,7 +12,7 @@ const init = (app, data) => {
     app.use('/api/Thread', router);
 
     router
-        .post('/createThread', async (req, res) => {
+        .post('/createThread', async(req, res) => {
             if (req.user) {
                 const {
                     threadTitle,
@@ -46,13 +46,12 @@ const init = (app, data) => {
             }
             res.redirect('/Category/' + req.body.catName);
         })
-        .get('/:id', async (req, res) => {
+        .get('/:id', async(req, res) => {
             const {
                 id,
             } = req.params;
 
-            res.locals.search = {
-                in: 'posts',
+            res.locals.search = { in: 'posts',
                 threadId: id,
             };
 
