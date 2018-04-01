53d563c49313d67bdceb21be962e853e0798bcdd
valio <Valios2@hotmail.com>
2018-04-01 03:12:44 +0300

    added description for controllers, added seeders and fixed minor bug in home pug

--- app/routes/threads/threads.controller.js
+++ app/routes/threads/threads.controller.js
@@ -3,27 +3,69 @@ class ThreadsController {
         this.data = data;
     }
 
+    /**
+     * @description Finds a Category by name
+     * @async
+     * @param {string} catName
+     * receives category name
+     * @return {Object} object with category info
+     */
     async getCategoryByCatName(catName) {
         return this.data.categories.getOneByCriteria({
             catName,
         });
     }
 
+
+    /**
+     * @description Crates a new Thread
+     * @async
+     * @param {Object} obj
+     * recevies an object with thread info and inserts it in database
+     * @return {Object} object of the inserted element
+     */
     async createThread(obj) {
         return this.data.threads.create(obj);
     }
 
+    /**
+     * @description Crates a new Post
+     * @async
+     * @param {Object} obj
+     * recevies an object with post info and inserts it in database
+     * @return {Object} object of the inserted element
+     */
     async createPost(obj) {
         return this.data.posts.create(obj);
     }
+
+    /**
+     * @description Finds all posts with give thread id
+     * @async
+     * @param {integer} id
+     * receives thread id
+     * @return {Array} Array with all posts in that Thread
+     */
     async getAllPostsByThreadId(id) {
         return this.data.posts.getAllByCriteria({
             ThreadId: +id,
         });
     }
+
+    /**
+     * @description Finds all Answers
+     * @async
+     * @return {Array} Array with all Answers
+     */
     async getAllAnswers() {
         return this.data.answers.getAll();
     }
+
+    /**
+     * @description Finds all Users
+     * @async
+     * @return {Array} Array with all Users
+     */
     async getAllUsers() {
         return this.data.users.getAll();
     }
--- db/seeders/4-threads.js
+++ db/seeders/4-threads.js
@@ -1,32 +1,39 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Threads', [{
-      id: 1,
-      title: 'Wheels',
-      createdAt: '2018-03-18 08:19:42',
-      updatedAt: '2018-03-18 08:20:50',
-      UserId: 1,
-      CategoryId: 1,
-    }, {
-      id: 2,
-      title: 'Flower Pots',
-      createdAt: '2018-03-18 08:19:42',
-      updatedAt: '2018-03-18 08:20:50',
-      UserId: 2,
-      CategoryId: 2,
-      }, {
-        id: 3,
-        title: 'World of Warcraft',
-        createdAt: '2018-03-18 08:19:42',
-        updatedAt: '2018-03-18 08:20:50',
-        UserId: 3,
-        CategoryId: 3,
-      }], {});
-  },
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('Threads', [{
+            id: 1,
+            title: 'Вентил за безкамерна капла',
+            createdAt: '2018-03-18 08:19:42',
+            updatedAt: '2018-03-18 08:20:50',
+            UserId: 1,
+            CategoryId: 1,
+        }, {
+            id: 2,
+            title: 'Какво да гледаме когато свършат любимите сериали',
+            createdAt: '2018-03-18 08:19:42',
+            updatedAt: '2018-03-18 08:20:50',
+            UserId: 2,
+            CategoryId: 2,
+        }, {
+            id: 3,
+            title: 'Отворен публичен дебат на страницата на Народното събрание ? ',
+            createdAt: '2018-03-18 08:19:42',
+            updatedAt: '2018-03-18 08:20:50',
+            UserId: 3,
+            CategoryId: 3,
+        }, {
+            id: 4,
+            title: 'Linux - Info, Guides, News, HOWTOs ...',
+            createdAt: '2018-03-18 08:19:42',
+            updatedAt: '2018-03-18 08:20:50',
+            UserId: 3,
+            CategoryId: 4,
+        }], {});
+    },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Threads', null, {});
-  }
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('Threads', null, {});
+    }
 };
